13212ae5065ad3a1426167e1bbc9d1c7d114f527
abrenzink <abrenzink@bonadmin.com>
2024-12-07 13:45:07 -0300

    minor fix

--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -2,9 +2,7 @@
 import {
   AtSymbolIcon,
   KeyIcon,
-  ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
 // import { Button } from './button';
 
 export default function LoginForm() {
